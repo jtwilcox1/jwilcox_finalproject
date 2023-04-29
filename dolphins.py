6c280679fb65a13e243f73a59d64fbbfcebc3990
jtwilcox1 <127146470+jtwilcox1@users.noreply.github.com>
2023-04-28 20:23:38 -0700

    Add files via upload

---
+++ dolphins.py
@@ -0,0 +1,16 @@
+# File created by JT Wilcox 
+# sources
+# https://www.scaler.com/topics/pandas/how-to-install-pandas-in-python/
+
+# # import libraries
+import os
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+project_dir = "drive/My Drive/Dolphins Prediction"
+szn_folders = os.listdr(f"{project_dir}/22-23 Season.pbp")
+
+
+
